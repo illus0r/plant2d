067e7e4a966389510180281124e38c880a4a0c0d
illus0r <zor667@gmail.com>
2017-07-15 22:27:47 +0300

    branching

--- plant2d.py
+++ plant2d.py
@@ -23,56 +23,74 @@ import random
 class Cell:
     parent = None
 
-    def __init__(self, position=[100,100], mass=1, parent=None, velocity=[0,0]):
-        position = [p + random.randrange(-5,5,1) for p in position]
+    def __init__(self, position=[100.0,100.0], mass=1, parent=None, velocity=[0.0,0.0]):
+        if parent:
+            position = [p + random.gauss(0,1) for p in position]
         self.timeCounter = {
                 'after_birth':0,
-                'after_child': 0
+                'after_child': 0,
                 }
-        self.bio_state = {
+        self.bioState = {
                 'isRoot': False, # ∞ mass
                 'hasChildren': False,
                 'size': 0,  # for both stems and berries
                 'width': 0, # for both stems and berries
                 'type':'stem',
-                'color':'#FFFFFF'
+                'color':'#FFFFFF',
             }
         self.dna = {
                 "space_required_for_reproduction": 20,
-                "time_before_reproduction": 40,
-                "chargeMultiplier": 0.1
+                "time_before_reproduction": 50,
+                "time_before_branching": 500,
+                "chargeMultiplier": 10,
+                "sizeMultiplier": 10,
                 } #TODO
         self.physics = {
                 "velocity":velocity[:],
                 "position":position[:],
                 "mass": mass,
-                "charge":0,
+                "charge": 0,
                 }
         self.physics["position"] = position
         self.parent = parent
         # import pdb;pdb.set_trace() 
 
-    def isReproductable(self):
-        return self.timeCounter["after_birth"] == self.dna["time_before_reproduction"]
+    # def is(self):
+        # if not self.parent:
+            # return self.timeCounter["after_birth"] == 0
+        # return self.timeCounter["after_birth"] == self.dna["time_before_reproduction"]
 
     def applyForce(self, force):
-        # print(self.physics["mass"])
         # root doesn't move:
         # if self.parent:
         self.physics["velocity"][0] += force[0] / self.physics["mass"]
         self.physics["velocity"][1] += force[1] / self.physics["mass"]
-        self.physics["position"][0] += self.physics["velocity"][0]
-        self.physics["position"][1] += self.physics["velocity"][1]
 
     def develop(self):
-        self.size = math.log10(self.timeCounter["after_birth"] + 2)
+        # physics
+        friction = 0.95
+        self.physics["position"][0] += self.physics["velocity"][0]
+        self.physics["position"][1] += self.physics["velocity"][1]
+        self.physics["velocity"] = [v*friction for v in self.physics["velocity"]]
+        #bio
+        self.bioState["size"] = self.dna["sizeMultiplier"] * \
+            math.log10(self.timeCounter["after_birth"]/5 + 1)
         self.physics["charge"] = self.dna["chargeMultiplier"] * \
-            math.log10(self.timeCounter["after_birth"] + 2)
+            math.log10(self.timeCounter["after_birth"] + 1)
+
+    def reproduceNumber(self):
+        if not self.parent:
+            if self.timeCounter["after_birth"] == 0:
+                return 5
+        if self.timeCounter["after_birth"] == self.dna["time_before_branching"]:
+            return 2
+        elif self.timeCounter["after_birth"] == self.dna["time_before_reproduction"]:
+            return 1
+        else:
+            return 0
 
     def reproduce(self):
-        childNumber = 2
-        if self.parent:
-            childNumber = 5
+        childNumber = self.reproduceNumber()
         return [Cell(
             position=self.physics["position"], 
             velocity=self.physics["velocity"], 
@@ -91,8 +109,7 @@ class Scene:
     # Потом доделать Параметры вьюпорта (проекция координат мира на экран)
 
     def __init__(self):
-        self.cells.append(Cell(position=[300,300], mass=100))
-        self.cells.append(Cell(position=[300,300], mass=50, parent=self.cells[0]))
+        self.cells.append(Cell(position=[300,300], mass=1000000000))
 
     def render(self):
         for cell in self.cells:
@@ -100,40 +117,120 @@ class Scene:
                 beginPoint = cell.getPosition()
                 endPoint = cell.parent.getPosition()
                 #print (beginPoint, endPoint)
-                arcade.draw_line(beginPoint[0], beginPoint[1], endPoint[0], endPoint[1], arcade.color.RED, 3)
+                arcade.draw_line(beginPoint[0], beginPoint[1], 
+                        endPoint[0], endPoint[1], 
+                        arcade.color.RED, 
+                        1)
+                        # 1+int(cell.bioState["size"]*4/cell.dna["sizeMultiplier"]))
 
     def develop(self):
         for i, cell in enumerate(self.cells):
             # physics =========================================================
             distMin = 1.0
-            otherCells = [c for c in self.cells[i+1:]]
-            for oCell in otherCells:
-                distX = cell.getPosition()[0] - oCell.getPosition()[0]
-                distY = cell.getPosition()[1] - oCell.getPosition()[1]
+            centerCharge = 5
+            junctureStrength = 0.1
+            # electric --------------------------------------------------------
+            distX = cell.getPosition()[0] - 300
+            distY = cell.getPosition()[1] - 300
+            distance = math.sqrt(distX**2 + distY**2)
+            if abs(distance) < distMin:
+                distance = distMin
+            force = (0.1 * cell.getCharge() * centerCharge) / distance
+            forceX = (force * distX) / distance
+            forceY = (force * distY) / distance
+            cell.applyForce((forceX,forceY))
+            # junction --------------------------------------------------------
+            if cell.parent:
+                distX = cell.getPosition()[0] - cell.parent.getPosition()[0]
+                distY = cell.getPosition()[1] - cell.parent.getPosition()[1]
                 distance = math.sqrt(distX**2 + distY**2)
                 if abs(distance) < distMin:
                     distance = distMin
-                force = (0.1 * cell.getCharge() * oCell.getCharge()) / distance**2
+                force = -junctureStrength * (distance - cell.bioState["size"])
                 forceX = (force * distX) / distance
                 forceY = (force * distY) / distance
+                forceParentX = -(force * distX) / distance
+                forceParentY = -(force * distY) / distance
                 cell.applyForce((forceX,forceY))
-                oCell.applyForce((-forceX,-forceY))
-                debugForceKoeff = 3000
-                arcade.draw_line(cell.getPosition()[0],
-                                 cell.getPosition()[1],
-                                 cell.getPosition()[0]+forceX*debugForceKoeff,
-                                 cell.getPosition()[1]+forceY*debugForceKoeff,
-                                 arcade.color.BLACK, 0.5)
-                arcade.draw_line(oCell.getPosition()[0],
-                                 oCell.getPosition()[1],
-                                 oCell.getPosition()[0]-forceX*debugForceKoeff,
-                                 oCell.getPosition()[1]-forceY*debugForceKoeff,
-                                 arcade.color.BLACK, 0.5)
+                cell.parent.applyForce((forceParentX,forceParentY))
+            # collisions ------------------------------------------------------
+            radiusSizeRatio = 1 # 0.5 → half size
+            collisionStrength = 0.01
+            otherCells = [c for c in self.cells[i+1:]]
+            radius = cell.bioState["size"] * radiusSizeRatio
+            for j, oCell in enumerate(otherCells):
+                if oCell != cell.parent:
+                    oRadius = oCell.bioState["size"] * radiusSizeRatio
+                    if oRadius<radius:
+                        radius = oRadius
+                    # print(radius)
+                    distX = cell.getPosition()[0] - oCell.getPosition()[0]
+                    distXAbs = abs(distX)
+                    if distXAbs < radius:
+                        distY = cell.getPosition()[1] - oCell.getPosition()[1]
+                        distYAbs = abs(distY)
+                        if distYAbs < radius:
+                            # print(i, i+1+j)
+                            # print("DistX, DistY: ", distX, distY)
+                            forceX = (distX/distXAbs) * \
+                                    (radius - distXAbs) * collisionStrength
+                            forceY = (distY/distYAbs) * \
+                                    (radius - distYAbs) * collisionStrength
+                            cell.applyForce((forceX,forceY))
+                            oCell.applyForce((-forceX,-forceY))
+                        # else:
+                            # print("too far Y")
+                    # else:
+                        # print("too far X")
+                else:
+                    print("parent")
+                debugForceKoeff = 300
+                # arcade.draw_line(cell.getPosition()[0],
+                                 # cell.getPosition()[1],
+                                 # cell.getPosition()[0]+forceX*debugForceKoeff,
+                                 # cell.getPosition()[1]+forceY*debugForceKoeff,
+                                 # arcade.color.BLACK, 0.5)
+                # arcade.draw_line(oCell.getPosition()[0],
+                                 # oCell.getPosition()[1],
+                                 # oCell.getPosition()[0]-forceX*debugForceKoeff,
+                                 # oCell.getPosition()[1]-forceY*debugForceKoeff,
+                                 # arcade.color.BLACK, 0.5)
+
+            # # electric force
+            # debugForceKoeff = 3000
+            # arcade.draw_line(cell.getPosition()[0],
+                             # cell.getPosition()[1],
+                             # cell.getPosition()[0]+forceX*debugForceKoeff,
+                             # cell.getPosition()[1]+forceY*debugForceKoeff,
+                             # arcade.color.BLACK, 0.5)
+
+            # otherCells = [c for c in self.cells[i+1:]]
+            # for oCell in otherCells:
+                # distX = cell.getPosition()[0] - oCell.getPosition()[0]
+                # distY = cell.getPosition()[1] - oCell.getPosition()[1]
+                # distance = math.sqrt(distX**2 + distY**2)
+                # if abs(distance) < distMin:
+                    # distance = distMin
+                # force = (0.1 * cell.getCharge() * oCell.getCharge()) / distance**2
+                # forceX = (force * distX) / distance
+                # forceY = (force * distY) / distance
+                # cell.applyForce((forceX,forceY))
+                # oCell.applyForce((-forceX,-forceY))
+                # debugForceKoeff = 3000
+                # arcade.draw_line(cell.getPosition()[0],
+                                 # cell.getPosition()[1],
+                                 # cell.getPosition()[0]+forceX*debugForceKoeff,
+                                 # cell.getPosition()[1]+forceY*debugForceKoeff,
+                                 # arcade.color.BLACK, 0.5)
+                # arcade.draw_line(oCell.getPosition()[0],
+                                 # oCell.getPosition()[1],
+                                 # oCell.getPosition()[0]-forceX*debugForceKoeff,
+                                 # oCell.getPosition()[1]-forceY*debugForceKoeff,
+                                 # arcade.color.BLACK, 0.5)
             #growth
             cell.develop()
             #reproduce
-            if cell.isReproductable():
-                self.cells.extend(cell.reproduce())
+            self.cells.extend(cell.reproduce())
             #increasing timer
             cell.timeCounter["after_birth"] += 1
 
@@ -160,7 +257,7 @@ def main():
     arcade.set_background_color(arcade.color.WHITE)
 
     # Tell the computer to call the draw command at the specified interval.
-    arcade.schedule(on_draw, 1/8)
+    arcade.schedule(on_draw, 1/80)
 
     # Run the program
     arcade.run()
